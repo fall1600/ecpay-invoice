78a020425d18d302aa155983607655ad089b00e2
fall1600 <fall1600@gmail.com>
2020-06-23 12:03:15 +0800

    cryption

--- Cryption.php
+++ Cryption.php
@@ -7,6 +7,15 @@ trait Cryption
     protected $cipher = 'aes-128-cbc';
 
     /**
+     * @param  string  $input
+     * @return string
+     */
+    public function countChecksum(string $input)
+    {
+        return strtolower(hash('sha256', $this->hashKey.$input.$this->hashIv));
+    }
+    
+    /**
      * 綠界說怎麼算就怎麼算
      * @param  array $data
      * @return string
@@ -34,5 +43,24 @@ trait Cryption
 
         $decrypted = openssl_decrypt($encrypted, $this->cipher, $this->hashKey, 0, $this->hashIv);
         return json_decode(urldecode($decrypted), true);
+    }
+
+    protected function replaceSymbol(string $str)
+    {
+        if(! empty($str)) {
+            $str = str_replace('%2D', '-', $str);
+            $str = str_replace('%2d', '-', $str);
+            $str = str_replace('%5F', '_', $str);
+            $str = str_replace('%5f', '_', $str);
+            $str = str_replace('%2E', '.', $str);
+            $str = str_replace('%2e', '.', $str);
+            $str = str_replace('%21', '!', $str);
+            $str = str_replace('%2A', '*', $str);
+            $str = str_replace('%2a', '*', $str);
+            $str = str_replace('%28', '(', $str);
+            $str = str_replace('%29', ')', $str);
+        }
+
+        return $str ;
     }
 }
