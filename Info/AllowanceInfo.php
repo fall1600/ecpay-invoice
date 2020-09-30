e72a6b0065953dfc426998e018081ea16cae5a44
fall1600 <fall1600@gmail.com>
2020-09-30 16:55:50 +0800

    開立發票日期為必填

--- Info/AllowanceInfo.php
+++ Info/AllowanceInfo.php
@@ -22,6 +22,12 @@ abstract class AllowanceInfo
     protected $vatType;
 
     /**
+     * 開立發票日期 (yyyy-MM-dd)
+     * @var string
+     */
+    protected $invoiceDate;
+
+    /**
      * @return array
      */
     abstract public function getInfo();
@@ -29,15 +35,18 @@ abstract class AllowanceInfo
     /**
      * 一律走線上折讓, 需要掛returnUrl, 用來收買家同意後綠界的webhook
      * AllowanceInfo constructor.
-     * @param  string  $invoiceNumber
-     * @param  string  $returnUrl
-     * @param  string  $vatType
+     * @param string $invoiceNumber
+     * @param string $returnUrl
+     * @param string $invoiceDate
+     * @param string $vatType
      */
-    public function __construct(string $invoiceNumber, string $returnUrl, string $vatType = VatType::YES)
+    public function __construct(string $invoiceNumber, string $returnUrl, string $invoiceDate, string $vatType = VatType::YES)
     {
         $this->invoiceNumber = $invoiceNumber;
 
         $this->returnUrl = $returnUrl;
+
+        $this->invoiceDate = $invoiceDate;
 
         $this->vatType = $vatType;
     }
